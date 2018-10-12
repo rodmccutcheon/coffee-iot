5e8cc06f00d7e3ddd7923c3ec0ec96c0549409d8
Rod McCutcheon <rodmccutcheon@gmail.com>
2018-10-12 21:33:38 +1100

    Initial commit.

---
+++ src/main/java/com/rodmccutcheon/coffeeiot/CoffeeIotApplication.java
@@ -0,0 +1,12 @@
+package com.rodmccutcheon.coffeeiot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeIotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeIotApplication.class, args);
+	}
+}
---
+++ src/test/java/com/rodmccutcheon/coffeeiot/CoffeeIotApplicationTests.java
@@ -0,0 +1,16 @@
+package com.rodmccutcheon.coffeeiot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CoffeeIotApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
